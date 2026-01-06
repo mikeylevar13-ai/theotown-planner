f7548b29e406019862c1007f14645d10a90f3b07
mikeylevar13-ai <mikeylevar13@gmail.com>
2026-01-06 08:41:39 -0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "theotown-planner-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+});
